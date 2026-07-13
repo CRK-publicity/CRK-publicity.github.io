4df658508eea016b0851d7109a280cca0da97f9b
CRK Publicity <96599054+IWANDEZU@users.noreply.github.com>
2026-07-13 01:53:57 -0500

    Rename public site and add print QR code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/crk-publicity-portfolio/',
+  base: '/crkpublicity/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
